8de7928ff478cd2f1167a7bf3e07bf56b451cd87
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-06-17 22:12:20 -0300

    First commit

---
+++ src/main/java/br/com/eLegis/ELegisApplication.java
@@ -0,0 +1,13 @@
+package br.com.eLegis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELegisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELegisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eLegis/ELegisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eLegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELegisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
