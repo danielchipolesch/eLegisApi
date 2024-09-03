f5d9a8219ae5621c4f3b78647371b212287505c7
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-02 23:49:22 -0300

    Delete and getById methods were created.

--- src/main/java/br/com/danielchipolesch/domain/services/DocumentationTypeService.java
+++ src/main/java/br/com/danielchipolesch/domain/services/DocumentationTypeService.java
@@ -1,7 +1,9 @@
 package br.com.danielchipolesch.domain.services;
 
+import br.com.danielchipolesch.domain.exceptions.DocumentationTypeException;
 import br.com.danielchipolesch.domain.dtos.documentationTypeDtos.DocumentationTypeCreateRequestDto;
 import br.com.danielchipolesch.domain.dtos.documentationTypeDtos.DocumentationTypeResponseDto;
+import br.com.danielchipolesch.domain.dtos.documentationTypeDtos.DocumentationTypeUpdateRequestDto;
 import br.com.danielchipolesch.domain.entities.documentationNumbering.DocumentationType;
 import br.com.danielchipolesch.infrastructure.repositories.DocumentationTypeRepository;
 import org.modelmapper.ModelMapper;
@@ -18,23 +20,45 @@ public class DocumentationTypeService {
     ModelMapper modelMapper;
 
     public DocumentationTypeResponseDto create(DocumentationTypeCreateRequestDto request) throws Exception {
-        DocumentationType documentationType = modelMapper.map(request, DocumentationType.class);
         if(documentationTypeRepository.existsByAcronym(request.acronym())){
-            throw new Exception("Espécie normativa já existe.");
+            throw new Exception(DocumentationTypeException.ALREADY_EXISTS.getMessage());
         }
 
+        DocumentationType documentationType = modelMapper.map(request, DocumentationType.class);
         documentationTypeRepository.save(documentationType);
         return modelMapper.map(documentationTypeRepository, DocumentationTypeResponseDto.class);
     }
 
-/*
-    public DocumentationTypeResponseDto update(DocumentationTypeUpdateRequestDto request) throws Exception{
-        /* TODO: Implementar código para atualizar uma espécia normativa * /
-        return;
-    }
-TODO: Implementar método para listar uma espécie normativa
-TODO: Implementar método para listar espécies normativas de forma paginada.
-TODO: Implementar método para excluir uma espécie normativa.
-*/
 
+    public DocumentationTypeResponseDto update(Long id, DocumentationTypeUpdateRequestDto request) throws Exception{
+
+        DocumentationType documentationType = documentationTypeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(DocumentationTypeException.NOT_FOUND.getMessage()));
+
+        documentationType.setAcronym(request.acronym().isBlank() ? documentationType.getAcronym() : request.acronym());
+        documentationType.setName(request.name().isBlank() ? documentationType.getName() : request.name());
+        documentationType.setDescription(request.description().isBlank() ? documentationType.getDescription() : request.description());
+
+        documentationTypeRepository.save(documentationType);
+
+        return modelMapper.map(documentationTypeRepository, DocumentationTypeResponseDto.class);
+    }
+
+    public DocumentationTypeResponseDto delete(Long id) throws Exception {
+        DocumentationType documentationType =
+                documentationTypeRepository.findById(id).orElseThrow(() -> new RuntimeException(DocumentationTypeException.NOT_FOUND.getMessage()));
+
+        documentationTypeRepository.delete(documentationType);
+
+        return modelMapper.map(documentationType, DocumentationTypeResponseDto.class);
+    }
+
+    public DocumentationTypeResponseDto getById(Long id) throws Exception {
+        DocumentationType documentationType =
+                documentationTypeRepository.findById(id).orElseThrow(() -> new RuntimeException(DocumentationTypeException.NOT_FOUND.getMessage()));
+
+        return modelMapper.map(documentationType, DocumentationTypeResponseDto.class);
+    }
+
+//  TODO: Implementar método para listar espécies normativas de forma paginada.
 }
