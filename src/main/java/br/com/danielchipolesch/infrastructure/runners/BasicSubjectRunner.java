e0b87962e46cd24c2f5e9b5024808f8c57f11829
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-04 17:24:15 -0300

    BasicSubjectRunner.java implementation was finished

--- src/main/java/br/com/danielchipolesch/infrastructure/runners/BasicSubjectRunner.java
+++ src/main/java/br/com/danielchipolesch/infrastructure/runners/BasicSubjectRunner.java
@@ -1,7 +1,29 @@
 package br.com.danielchipolesch.infrastructure.runners;
 
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import br.com.danielchipolesch.infrastructure.enums.BasicSubjectEnum;
+import br.com.danielchipolesch.infrastructure.repositories.BasicSubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BasicSubjectRunner {
+public class BasicSubjectRunner implements CommandLineRunner {
+
+    @Autowired
+    BasicSubjectRepository basicSubjectRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        BasicSubjectEnum[] basicSubjectEnums = BasicSubjectEnum.values();
+        for (BasicSubjectEnum basicSubjectEnum : basicSubjectEnums){
+            if (!basicSubjectRepository.existsByNumber(basicSubjectEnum.toString())){
+                BasicSubject basicSubject = new BasicSubject();
+                basicSubject.setNumber(basicSubjectEnum.getNumber());
+                basicSubject.setName(basicSubjectEnum.getName());
+                basicSubject.setDescription(basicSubjectEnum.getDescription());
+                basicSubjectRepository.save(basicSubject);
+            }
+        }
+    }
 }
