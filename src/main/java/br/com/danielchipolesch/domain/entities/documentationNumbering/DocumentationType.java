c25e8917495ebc4a64ce0bfb159959c9cd6a7921
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-03 22:54:24 -0300

    Code smell improvement.

--- src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/DocumentationType.java
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/DocumentationType.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
@@ -16,6 +17,7 @@ public class DocumentationType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_especie_normativa")
     private Long id;
 
     @Column(name = "sg_especie_normativa", nullable = false)
@@ -24,16 +26,16 @@ public class DocumentationType {
     @Column(name = "nm_especie_normativa", nullable = false)
     private String name;
 
-    @Column(name = "tx_descricao", nullable = false)
+    @Column(name = "tx_descricao", nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(name = "dt_criacao", updatable = false)
     @CreationTimestamp
     private Timestamp createdAt;
 
-    @Column(name = "dt_edicao")
+    @Column(name = "dt_alteracao")
     @UpdateTimestamp
-    private Timestamp editedAt;
+    private Timestamp updatedAt;
 
     @Version
     @Column(name = "nr_versao", nullable = false)
