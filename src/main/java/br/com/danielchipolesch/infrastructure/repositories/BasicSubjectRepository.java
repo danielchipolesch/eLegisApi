82c7794cd93a8ff24d1435e822fe1ac64dfe1907
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-04 17:23:58 -0300

    BasicSubjectRepository.java implementation was finished

---
+++ src/main/java/br/com/danielchipolesch/infrastructure/repositories/BasicSubjectRepository.java
@@ -0,0 +1,10 @@
+package br.com.danielchipolesch.infrastructure.repositories;
+
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasicSubjectRepository extends JpaRepository<BasicSubject, Long> {
+    boolean existsByNumber(String basicSubjectNumber);
+}
