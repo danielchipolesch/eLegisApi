c1a27c37411ced9f909618d70e0b1c19483dee44
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-04 17:22:04 -0300

    BasicSubject.java entity was finished

--- src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/BasicSubject.java
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/BasicSubject.java
@@ -1,7 +1,40 @@
 package br.com.danielchipolesch.domain.entities.documentationNumbering;
 
-import jakarta.persistence.Entity;
-import org.springframework.stereotype.Service;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "t_assunto_basico")
 public class BasicSubject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_assunto_basico")
+    private Long id;
+
+    @Column(name = "nr_assunto_basico", length = 4, nullable = false)
+    private String number;
+
+    @Column(name = "nm_classificacao", nullable = false)
+    private String name;
+
+    @Column(name = "tx_descricao", columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @Column(name = "dt_criacao", updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "dt_alteracao")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Version
+    @Column(name = "nr_versao", nullable = false)
+    private Integer version;
 }
