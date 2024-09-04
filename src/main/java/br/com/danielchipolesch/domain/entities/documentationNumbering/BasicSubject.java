7187d693708e5fb245d68715fc6d88a3fb5e5770
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-03 22:52:48 -0300

    @Service annotation was removed because an @Id annotation is needed when that's present.

--- src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/BasicSubject.java
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentationNumbering/BasicSubject.java
@@ -1,7 +1,7 @@
 package br.com.danielchipolesch.domain.entities.documentationNumbering;
 
+import jakarta.persistence.Entity;
 import org.springframework.stereotype.Service;
 
-@Service
 public class BasicSubject {
 }
