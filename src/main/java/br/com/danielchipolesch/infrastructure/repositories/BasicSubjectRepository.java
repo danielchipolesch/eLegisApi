9550ec7ca9b8606f579f79193855d712e1cd812f
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-10 21:31:18 -0300

    BasicSubject findByNumber(String number) method was created on BasicSubjectRepository.java to help on finding basic subjects by number

--- src/main/java/br/com/danielchipolesch/infrastructure/repositories/BasicSubjectRepository.java
+++ src/main/java/br/com/danielchipolesch/infrastructure/repositories/BasicSubjectRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BasicSubjectRepository extends JpaRepository<BasicSubject, Long> {
     boolean existsByNumber(String basicSubjectNumber);
+    BasicSubject findByNumber(String number);
 }
