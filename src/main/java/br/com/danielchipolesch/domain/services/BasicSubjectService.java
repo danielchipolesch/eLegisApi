7f7e6dcedea4399a8688b7e9ec21d1a5ca579be0
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-10 21:36:12 -0300

    BasicSubjectService.java was created successfully

---
+++ src/main/java/br/com/danielchipolesch/domain/services/BasicSubjectService.java
@@ -0,0 +1,81 @@
+package br.com.danielchipolesch.domain.services;
+
+import br.com.danielchipolesch.domain.dtos.basicSubjectDtos.BasicSubjectRequestCreateDto;
+import br.com.danielchipolesch.domain.dtos.basicSubjectDtos.BasicSubjectRequestUpdateDto;
+import br.com.danielchipolesch.domain.dtos.basicSubjectDtos.BasicSubjectResponseDto;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import br.com.danielchipolesch.domain.exceptions.BasicSubjectException;
+import br.com.danielchipolesch.infrastructure.repositories.BasicSubjectRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BasicSubjectService {
+
+    @Autowired
+    BasicSubjectRepository basicSubjectRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public BasicSubjectResponseDto create(BasicSubjectRequestCreateDto request) throws Exception {
+        if(basicSubjectRepository.existsByNumber(request.getNumber())){
+            throw new Exception(BasicSubjectException.ALREADY_EXISTS.getMessage());
+        }
+
+        BasicSubject basicSubject = modelMapper.map(request, BasicSubject.class);
+        basicSubjectRepository.save(basicSubject);
+        return modelMapper.map(basicSubject, BasicSubjectResponseDto.class);
+    }
+
+
+    public BasicSubjectResponseDto update(Long id, BasicSubjectRequestUpdateDto request) throws Exception{
+
+        BasicSubject basicSubject = basicSubjectRepository.findById(id)
+                .orElseThrow(() -> new Exception(BasicSubjectException.NOT_FOUND.getMessage()));
+
+        basicSubject.setNumber(request.getNumber().isBlank() ? basicSubject.getNumber() : request.getNumber());
+        basicSubject.setName(request.getName().isBlank() ? basicSubject.getName() : request.getName());
+        basicSubject.setDescription(request.getDescription().isBlank() ? basicSubject.getDescription() : request.getDescription());
+
+        basicSubjectRepository.save(basicSubject);
+
+        return modelMapper.map(basicSubject, BasicSubjectResponseDto.class);
+    }
+
+    public BasicSubjectResponseDto delete(Long id) throws Exception {
+        BasicSubject basicSubject = basicSubjectRepository.findById(id)
+                .orElseThrow(() -> new Exception(BasicSubjectException.NOT_FOUND.getMessage()));
+
+        basicSubjectRepository.delete(basicSubject);
+
+        return modelMapper.map(basicSubject, BasicSubjectResponseDto.class);
+    }
+
+    public BasicSubjectResponseDto getById(Long id) throws Exception {
+        BasicSubject basicSubject = basicSubjectRepository.findById(id)
+                .orElseThrow(() -> new Exception(BasicSubjectException.NOT_FOUND.getMessage()));
+
+        return modelMapper.map(basicSubject, BasicSubjectResponseDto.class);
+    }
+
+    public BasicSubjectResponseDto getByNumber(String number) throws Exception {
+        BasicSubject basicSubject = basicSubjectRepository.findByNumber(number);
+//                .orElseThrow(() -> new Exception(BasicSubjectException.NOT_FOUND.getMessage()));
+
+        return modelMapper.map(basicSubject, BasicSubjectResponseDto.class);
+    }
+
+    public List<BasicSubjectResponseDto> getAll(Pageable pageable) throws Exception {
+        Page<BasicSubject> basicSubjects = basicSubjectRepository.findAll(pageable);
+
+        List<BasicSubjectResponseDto> responseDtos = basicSubjects.stream().map(basicSubject -> modelMapper.map(basicSubject, BasicSubjectResponseDto.class)).toList();
+
+        return responseDtos;
+    }
+}
