89c5d474d63746cafa9ba004ad8d39c04a5c69d6
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-12 22:00:23 -0300

    DocumentAttachment.java entity was created

---
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentStructure/DocumentAttachment.java
@@ -0,0 +1,21 @@
+package br.com.danielchipolesch.domain.entities.documentStructure;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "") // Table name must me created
+@Data
+public class DocumentAttachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_anexo_documento")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String textDocument;
+
+    @OneToOne(mappedBy = "documentAttachment")
+    private Document document;
+}
