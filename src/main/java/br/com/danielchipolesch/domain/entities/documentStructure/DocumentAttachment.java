4c5ef40927d12301e554f6ff1549daca7956b97f
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-16 20:39:00 -0300

    Table renamed.

--- src/main/java/br/com/danielchipolesch/domain/entities/documentStructure/DocumentAttachment.java
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentStructure/DocumentAttachment.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "") // Table name must be created
+@Table(name = "t_anexo_documento") // Table name must be created
 @Data
 public class DocumentAttachment {
 
@@ -13,9 +13,10 @@ public class DocumentAttachment {
     @Column(name = "id_anexo_documento")
     private Long id;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(name = "tx_documento", columnDefinition = "TEXT")
     private String textDocument;
 
     @OneToOne(mappedBy = "documentAttachment")
     private Document document;
+
 }
