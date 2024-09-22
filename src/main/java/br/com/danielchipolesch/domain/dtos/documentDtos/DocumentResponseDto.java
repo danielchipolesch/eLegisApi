f3512e5ec051eccb35e3e4518c08126b2dddcb12
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-22 00:14:39 -0300

    DocumentResponseDto.java was updated with correct params.

--- src/main/java/br/com/danielchipolesch/domain/dtos/documentDtos/DocumentResponseDto.java
+++ src/main/java/br/com/danielchipolesch/domain/dtos/documentDtos/DocumentResponseDto.java
@@ -1,8 +1,39 @@
 package br.com.danielchipolesch.domain.dtos.documentDtos;
 
+import br.com.danielchipolesch.domain.entities.documentStructure.DocumentAct;
+import br.com.danielchipolesch.domain.entities.documentStructure.DocumentAttachment;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.DocumentationType;
+import br.com.danielchipolesch.domain.services.DocumentStatusEnum;
 import lombok.Data;
+
+
+import java.sql.Timestamp;
 
 @Data
 public class DocumentResponseDto {
-    //TODO Create attributes
+
+    private Long id;
+
+    private BasicSubject basicSubject;
+
+    private DocumentationType documentationType;
+
+    private Integer secondaryNumber;
+
+    private String documentTitle;
+
+    private DocumentStatusEnum documentStatusEnum;
+
+    private DocumentAct documentAct;
+
+    private DocumentAttachment documentAttachment;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    private Integer version;
+
+
 }
