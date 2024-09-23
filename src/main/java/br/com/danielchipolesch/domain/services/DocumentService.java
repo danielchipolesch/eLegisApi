d4508be6e1733e64daf40d54ce15f108047da755
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-22 23:23:47 -0300

    Package imports

--- src/main/java/br/com/danielchipolesch/domain/services/DocumentService.java
+++ src/main/java/br/com/danielchipolesch/domain/services/DocumentService.java
@@ -2,9 +2,30 @@ package br.com.danielchipolesch.domain.services;
 
 import br.com.danielchipolesch.domain.dtos.documentDtos.DocumentRequestCreateDto;
 import br.com.danielchipolesch.domain.dtos.documentDtos.DocumentResponseDto;
+import br.com.danielchipolesch.domain.dtos.documentDtos.DocumentUpdateDocumentAttachmentRequestDto;
+import br.com.danielchipolesch.domain.entities.documentStructure.Document;
+import br.com.danielchipolesch.domain.entities.documentStructure.DocumentAttachment;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.DocumentationType;
+import br.com.danielchipolesch.domain.exceptions.ResourceCannotBeUpdatedException;
+import br.com.danielchipolesch.domain.exceptions.enums.BasicSubjectException;
+import br.com.danielchipolesch.domain.exceptions.enums.DocumentAttachmentException;
+import br.com.danielchipolesch.domain.exceptions.enums.DocumentException;
+import br.com.danielchipolesch.domain.exceptions.enums.DocumentationTypeException;
+import br.com.danielchipolesch.application.helpers.DocumentMapper;
+import br.com.danielchipolesch.domain.exceptions.ResourceNotFoundException;
+import br.com.danielchipolesch.infrastructure.repositories.BasicSubjectRepository;
+import br.com.danielchipolesch.infrastructure.repositories.DocumentAttachmentRepository;
 import br.com.danielchipolesch.infrastructure.repositories.DocumentRepository;
+import br.com.danielchipolesch.infrastructure.repositories.DocumentationTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DocumentService {
