cc8757189c2ff17f919937b16cfb349c8a4afd88
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-22 23:13:00 -0300

    This annotation avoid infinity recursive on JSON response. It was necessary even though these relationships are unidirectional.

--- src/main/java/br/com/danielchipolesch/domain/entities/documentStructure/Document.java
+++ src/main/java/br/com/danielchipolesch/domain/entities/documentStructure/Document.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "t_documento")
 @Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Document {
 
     @Id
