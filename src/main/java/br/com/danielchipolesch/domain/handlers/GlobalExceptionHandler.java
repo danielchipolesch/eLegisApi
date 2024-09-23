4403f2f5fe5799e7337c60970187af68b0f750b4
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-22 22:56:51 -0300

    Created to catch exceptions globally.

---
+++ src/main/java/br/com/danielchipolesch/domain/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package br.com.danielchipolesch.domain.handlers;
+
+import br.com.danielchipolesch.domain.exceptions.InvalidInputException;
+import br.com.danielchipolesch.domain.exceptions.ResourceAlreadyExistsException;
+import br.com.danielchipolesch.domain.exceptions.ResourceCannotBeUpdatedException;
+import br.com.danielchipolesch.domain.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now().toString());
+        map.put("message", e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFound(ResourceNotFoundException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now().toString());
+        map.put("message", e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<Map<String, Object>> handleAlreadyExists(ResourceAlreadyExistsException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now().toString());
+        map.put("message", e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidInput(InvalidInputException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now().toString());
+        map.put("message", e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(ResourceCannotBeUpdatedException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceCannotBeUpdatedException(ResourceCannotBeUpdatedException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now().toString());
+        map.put("message", e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.FORBIDDEN);
+    }
+}
