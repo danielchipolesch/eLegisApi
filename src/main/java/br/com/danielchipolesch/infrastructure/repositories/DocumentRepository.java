d663ce514e64ef9f63502284ace08a5fa3e81f90
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-22 23:16:58 -0300

    This method allowed find documents with same documentations type and same basic subject and then automatically increment secondary number.

--- src/main/java/br/com/danielchipolesch/infrastructure/repositories/DocumentRepository.java
+++ src/main/java/br/com/danielchipolesch/infrastructure/repositories/DocumentRepository.java
@@ -1,9 +1,16 @@
 package br.com.danielchipolesch.infrastructure.repositories;
 
 import br.com.danielchipolesch.domain.entities.documentStructure.Document;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.BasicSubject;
+import br.com.danielchipolesch.domain.entities.documentationNumbering.DocumentationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+//   @Query("select u from Document u where u.id_especie_normativa = ?1 and u.id_assunto_basico = ?2") -> Não está funcionando
+   List<Document> findByDocumentationTypeAndBasicSubject(DocumentationType documentationType, BasicSubject basicSubject);
+   List<Document> findByDocumentationType(DocumentationType documentationType);
 }
