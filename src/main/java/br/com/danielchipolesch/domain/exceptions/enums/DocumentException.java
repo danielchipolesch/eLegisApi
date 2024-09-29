d56d5b15524efd24c3f74afac37c06e8ac0faec3
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-09-29 00:36:45 -0300

    Elimination of unnecessary methods and creation of new constants to store exception messages for each status.

--- src/main/java/br/com/danielchipolesch/domain/exceptions/enums/DocumentException.java
+++ src/main/java/br/com/danielchipolesch/domain/exceptions/enums/DocumentException.java
@@ -1,29 +1,26 @@
 package br.com.danielchipolesch.domain.exceptions.enums;
 
-import br.com.danielchipolesch.domain.services.DocumentStatusEnum;
+import br.com.danielchipolesch.domain.entities.documentStructure.DocumentStatus;
 import lombok.Getter;
 
 @Getter
 public enum DocumentException {
 
-    NOT_FOUND("Documento não encontrado"),
-    ALREADY_EXISTS("Documento já existe"),
-    CANNOT_BE_UPDATED("Documento não está em MINUTA ou RASCUNHO");
+    NOT_FOUND("Documento não encontrado."),
+    ALREADY_EXISTS("Documento já existe."),
+    CANNOT_BE_UPDATED("Documento não está em MINUTA ou RASCUNHO."),
+    RASCUNHO("Para serem aprovados os documentos devem estar como rascunho ou minuta."),
+    MINUTA("Para serem aprovados os documentos devem estar como rascunho ou minuta."),
+    APROVADO ("Documentos está aprovado e não pode ser alterado."),
+    PUBLICADO("Documentos publicados não podem ser alterados."),
+    ARQUIVADO("Documentos arquivados não podem ser alterados."),
+    CANCELADO("Documentos cancelados não podem ser alterados."),
+    REVOGADO("Documento está revogado");
+
 
     private final String message;
 
     DocumentException(String message) {
         this.message = message;
-    }
-
-    public static String cannotUpdateForStatus(DocumentStatusEnum status) {
-        return switch (status) {
-            case APROVADO -> "Documentos aprovados não podem ser alterados.";
-            case PUBLICADO -> "Documentos publicados não podem ser alterados.";
-            case ARQUIVADO -> "Documentos arquivados não podem ser alterados.";
-            case CANCELADO -> "Documentos cancelados não podem ser alterados.";
-            case REVOGADO -> "Documentos revogados não podem ser alterados";
-            default -> "Estado inválido para atualização.";
-        };
     }
 }
