762a7da73ba325d3185304379c77bf9b983bdb9a
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-10-16 19:45:03 -0300

    Exception was changed by RuntimeException

--- src/main/java/br/com/danielchipolesch/application/controllers/DocumentController.java
+++ src/main/java/br/com/danielchipolesch/application/controllers/DocumentController.java
@@ -56,24 +56,24 @@ public class DocumentController {
                     }
             )
     })
-    public ResponseEntity<DocumentResponseDto> post(@RequestBody @Valid DocumentRequestCreateDto request) throws Exception {
+    public ResponseEntity<DocumentResponseDto> post(@RequestBody @Valid DocumentRequestCreateDto request) throws RuntimeException {
         return ResponseEntity.status(HttpStatus.CREATED).body(documentService.create(request));
     }
 
     @PostMapping("{id}/clone")
-    public ResponseEntity<DocumentResponseDto> clone(@PathVariable(value = "id") Long id) throws Exception {
+    public ResponseEntity<DocumentResponseDto> clone(@PathVariable(value = "id") Long id) throws RuntimeException {
         return ResponseEntity.status(HttpStatus.CREATED).body(documentService.clone(id));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<DocumentResponseDto> getById(@PathVariable(value = "id") Long id) throws Exception{
+    public ResponseEntity<DocumentResponseDto> getById(@PathVariable(value = "id") Long id) throws RuntimeException{
         return ResponseEntity.status(HttpStatus.OK).body(documentService.getById(id));
     }
 
     @GetMapping("/especie-normativa/{documentTypeId}/assunto-basico/{basicSubjectId}")
     public ResponseEntity<List<DocumentResponseDto>> getByDocumentationTypeAndBasicSubject(
             @PathVariable(value = "documentTypeId") Long documentTypeId,
-            @PathVariable(value = "basicSubjectId") Long basicSubjectId) throws Exception {
+            @PathVariable(value = "basicSubjectId") Long basicSubjectId) throws RuntimeException {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.getByDocumentationTypeAndBasicSubject(documentTypeId, basicSubjectId));
     }
 
@@ -81,7 +81,7 @@ public class DocumentController {
     public ResponseEntity<List<DocumentResponseDto>> getAll(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy) throws Exception {
+            @RequestParam(defaultValue = "id") String sortBy) throws RuntimeException {
 
 
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
@@ -90,7 +90,7 @@ public class DocumentController {
 
     @PutMapping("{id}/anexo")
     public ResponseEntity<DocumentResponseDto> putDocumentAttachmentDocument(@PathVariable(value = "id") Long id,
-                                                                             @RequestBody DocumentUpdateDocumentAttachmentRequestDto request) throws Exception {
+                                                                             @RequestBody DocumentUpdateDocumentAttachmentRequestDto request) throws RuntimeException {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.updateDocumentAttachment(id, request));
     }
 
@@ -100,7 +100,7 @@ public class DocumentController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<DocumentResponseDto> delete(@PathVariable(value = "id") Long id) throws Exception {
+    public ResponseEntity<DocumentResponseDto> delete(@PathVariable(value = "id") Long id) throws RuntimeException {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(documentService.delete(id));
     }
 }
