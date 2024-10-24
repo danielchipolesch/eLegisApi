b1caa6bfc60b7f0531a64a6ac0a6f8b7f861d8ac
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-10-24 18:30:44 -0300

    DocumentAttachmentService.java was created

--- src/main/java/br/com/danielchipolesch/domain/services/DocumentAttachmentService.java
+++ src/main/java/br/com/danielchipolesch/domain/services/DocumentAttachmentService.java
@@ -1,7 +1,25 @@
 package br.com.danielchipolesch.domain.services;
 
+import br.com.danielchipolesch.application.dtos.documentAttachmentDtos.DocumentAttachmentResponseDto;
+import br.com.danielchipolesch.domain.entities.documentStructure.Document;
+import br.com.danielchipolesch.domain.entities.documentStructure.DocumentAttachment;
+import br.com.danielchipolesch.domain.handlers.exceptions.ResourceNotFoundException;
+import br.com.danielchipolesch.domain.handlers.exceptions.enums.DocumentAttachmentException;
+
+import br.com.danielchipolesch.domain.mappers.DocumentAttachmentMapper;
+import br.com.danielchipolesch.infrastructure.repositories.DocumentAttachmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocumentAttachmentService {
+
+    @Autowired
+    DocumentAttachmentRepository documentAttachmentRepository;
+
+    public DocumentAttachmentResponseDto getById(Long id) throws RuntimeException{
+
+        DocumentAttachment documentAttachment = documentAttachmentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(DocumentAttachmentException.NOT_FOUND.getMessage()));
+        return DocumentAttachmentMapper.documentAttachmentToDocumentAttachmentResponseDto(documentAttachment);
+    }
 }
