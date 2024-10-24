4b1f2397d345bf184390b14cb25aecf41397b19b
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-10-24 19:05:17 -0300

    RegulatoryActRepository.java new function to get an regulatoryAct by documentId.

--- src/main/java/br/com/danielchipolesch/infrastructure/repositories/RegulatoryActRepository.java
+++ src/main/java/br/com/danielchipolesch/infrastructure/repositories/RegulatoryActRepository.java
@@ -1,9 +1,11 @@
 package br.com.danielchipolesch.infrastructure.repositories;
 
+import br.com.danielchipolesch.domain.entities.documentStructure.Document;
 import br.com.danielchipolesch.domain.entities.documentStructure.RegulatoryAct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegulatoryActRepository extends JpaRepository<RegulatoryAct, Long> {
+    RegulatoryAct findByDocumentId(Document document);
 }
