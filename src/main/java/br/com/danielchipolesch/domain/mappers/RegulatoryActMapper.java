d908e9841a404297f387d3c13a2e0f6f093eae39
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-10-28 23:45:22 -0300

    Three new useful mappers on RegulatoryActMapper.java

--- src/main/java/br/com/danielchipolesch/domain/mappers/RegulatoryActMapper.java
+++ src/main/java/br/com/danielchipolesch/domain/mappers/RegulatoryActMapper.java
@@ -1,16 +1,43 @@
 package br.com.danielchipolesch.domain.mappers;
 
+import br.com.danielchipolesch.application.dtos.regulatoryActDtos.RegulatoryActDto;
 import br.com.danielchipolesch.application.dtos.regulatoryActDtos.RegulatoryActResponseDto;
+import br.com.danielchipolesch.application.dtos.regulatoryActDtos.RegulatoryActResponseNoPdfDto;
 import br.com.danielchipolesch.domain.entities.documentStructure.RegulatoryAct;
 
 public class RegulatoryActMapper {
 
-    public static RegulatoryActResponseDto regulatoryActToResponseActResponseDto(RegulatoryAct regulatoryAct){
+    public static RegulatoryActResponseNoPdfDto regulatoryActToRegulatoryActResponseNoPdfDto(RegulatoryAct regulatoryAct){
+        return new RegulatoryActResponseNoPdfDto(
+                regulatoryAct.getId(),
+                DocumentMapper.documentToDocumentResponseDto(regulatoryAct.getDocument()),
+                regulatoryAct.getFileName()
+        );
+    }
+
+    public static RegulatoryActResponseDto regulatoryActToRegulatoryActResponseDto(RegulatoryAct regulatoryAct) {
         return new RegulatoryActResponseDto(
                 regulatoryAct.getId(),
-                regulatoryAct.getDocument(),
+                DocumentMapper.documentToDocumentResponseDto(regulatoryAct.getDocument()),
                 regulatoryAct.getFileName(),
                 regulatoryAct.getData()
+        );
+    }
+
+    public static RegulatoryActDto regulatoryActToRegulatoryActDto(RegulatoryAct regulatoryAct){
+        return new RegulatoryActDto(
+                regulatoryAct.getId(),
+                DocumentMapper.documentToDocumentResponseDto(regulatoryAct.getDocument()),
+                regulatoryAct.getFileName(),
+                regulatoryAct.getData()
+        );
+    }
+
+    public static RegulatoryActResponseNoPdfDto regulatoryActDtoToRegulatoryActResponseNoPdfDto(RegulatoryActDto regulatoryActDto){
+        return new RegulatoryActResponseNoPdfDto(
+                regulatoryActDto.getIdPortaria(),
+                regulatoryActDto.getDocumento(),
+                regulatoryActDto.getNomePortaria()
         );
     }
 }
