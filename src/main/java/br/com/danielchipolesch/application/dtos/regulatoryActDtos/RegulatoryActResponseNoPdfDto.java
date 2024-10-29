3901c61dcc59c72aa2ba669d3b493dbc2306d239
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-10-28 23:45:55 -0300

    New useful Dto

---
+++ src/main/java/br/com/danielchipolesch/application/dtos/regulatoryActDtos/RegulatoryActResponseNoPdfDto.java
@@ -0,0 +1,14 @@
+package br.com.danielchipolesch.application.dtos.regulatoryActDtos;
+
+import br.com.danielchipolesch.application.dtos.documentDtos.DocumentResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegulatoryActResponseNoPdfDto {
+
+    private Long idPortaria;
+    private DocumentResponseDto documento;
+    private String nomePortaria;
+}
