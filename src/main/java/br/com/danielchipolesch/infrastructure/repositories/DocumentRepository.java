b790a72058a2c69d969c02d424a65f1e20956e74
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2025-01-07 19:46:02 -0300

    One day I will achieve a way to return a document by its documentation type and basic subject. The error occur when the document has a regulatory act associated.

--- src/main/java/br/com/danielchipolesch/infrastructure/repositories/DocumentRepository.java
+++ src/main/java/br/com/danielchipolesch/infrastructure/repositories/DocumentRepository.java
@@ -10,7 +10,17 @@ import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-//   @Query("select u from Document u where u.id_especie_normativa = ?1 and u.id_assunto_basico = ?2") -> Não está funcionando
+//   @Query("SELECT d.id, d.documentStatus from Document d where d.documentationType = 3 and d.basicSubject = 5") // -> Não está funcionando
+//   @Query(value = "SELECT d.id_documento, d.nm_titulo_documento, d.id_especie_normativa, d.id_assunto_basico, d.nr_numero_secundario, d.st_documento FROM t_documento d WHERE d.id_especie_normativa = ?1 and d.id_assunto_basico = ?2", nativeQuery = true) // -> Não está funcionando
+//   @Query("SELECT d.id FROM Document d WHERE d.documentationType = ?1 AND d.basicSubject = ?2")
+//   List<Document> findByDocumentationTypeAndBasicSubject(DocumentationType documentationType, BasicSubject basicSubject);
+
+//   @EntityGraph(attributePaths = {"documentTextAttachment"})
    List<Document> findByDocumentationTypeAndBasicSubject(DocumentationType documentationType, BasicSubject basicSubject);
+
+//   @Query("SELECT d FROM Document d LEFT JOIN FETCH d.documentAttachment da WHERE d.documentationType = :documentationType AND d.basicSubject = :basicSubject")
+//   List<Document> findByDocumentationTypeAndBasicSubject(@Param("documentationType") DocumentationType documentationType, @Param("basicSubject") BasicSubject basicSubject);
+
+
    List<Document> findByDocumentationType(DocumentationType documentationType);
 }
